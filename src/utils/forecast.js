4b77074e562ebecabbe133c6df112e6df640d993
EnZo1826 <jacohanekom90@gmail.com>
2020-08-14 16:09:02 +0200

    add humidity to weather string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,13 +3,14 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=a5f08c89a6db6ad2c9a05cb1ff0724dc&query=' + latitude + ',' + longitude + '&units=m'
 
-    request({ url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ' It is currently ' + response.body.current.temperature + ' degress out. It feels like ' + response.body.current.feelslike)
+            console.log(body.current)
+            callback(undefined, body.current.weather_descriptions[0] + " It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degrees out." + " The humidity is " + body.current.humidity + "%")
         }
     })
 }
